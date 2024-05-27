5df214fd791d3446b30dd17a3892ac5805aaa215
MYSELF-SAYAN <116438215+MYSELF-SAYAN@users.noreply.github.com>
2024-05-27 11:11:18 +0530

    Spring security files disabled

--- src/main/java/blog/app/service/UserdetailsService.java
+++ src/main/java/blog/app/service/UserdetailsService.java
@@ -1,30 +1,30 @@
-package blog.app.service;
-
-import blog.app.model.User;
-import blog.app.repository.AuthRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
-
-@Component
-public class UserdetailsService implements UserDetailsService {
-    @Autowired
-    private AuthRepository authRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = authRepository.findByUsername(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
-        else{
-          UserDetails userDetails=  org.springframework.security.core.userdetails.User.builder()
-                    .username(user.getUsername())
-                    .password(user.getPassword())
-                    .build();
-          return userDetails;
-        }
-    }
-}
+//package blog.app.service;
+//
+//import blog.app.model.User;
+//import blog.app.repository.AuthRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class UserdetailsService implements UserDetailsService {
+//    @Autowired
+//    private AuthRepository authRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        User user = authRepository.findByUsername(username);
+//        if (user == null) {
+//            throw new UsernameNotFoundException("User not found");
+//        }
+//        else{
+//          UserDetails userDetails=  org.springframework.security.core.userdetails.User.builder()
+//                    .username(user.getUsername())
+//                    .password(user.getPassword())
+//                    .build();
+//          return userDetails;
+//        }
+//    }
+//}
